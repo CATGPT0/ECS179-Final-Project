a9ff6435aa50c639b2cd134e7b0d14f43d1dd3a1
lizi23300 <jzgli@ucdavis.edu>
2024-02-28 19:59:37 -0800

    ll

---
+++ Secret of the Land/Assets/Scripts/CardBattleScripts/Enemy.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    int HP = 10;
+    int maxAttackDamage = 5; // Inclusive
+    int minAttackDamage = 3; // Inclusive
+}
