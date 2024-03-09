d8a7090659878d136f8a35b7bae361af9ece2ffa
CATGPT0 <133710245+CATGPT0@users.noreply.github.com>
2024-03-08 18:45:11 -0800

    Update Spawn

--- Secret of the Land/Assets/Scripts/Manager/SpawnManager.cs
+++ Secret of the Land/Assets/Scripts/Manager/SpawnManager.cs
@@ -6,10 +6,18 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject skeletonPrefab;
+    [SerializeField]
+    private Transform topLeft;
+    [SerializeField]
+    private Transform bottomRight;
 
     void Start()
     {
-        
+        for (int i = 0; i < 5; i++)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(topLeft.position.x, bottomRight.position.x), Random.Range(bottomRight.position.y, topLeft.position.y), 0);
+            Instantiate(skeletonPrefab, spawnPosition, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
@@ -17,4 +25,5 @@ public class SpawnManager : MonoBehaviour
     {
         
     }
+
 }
