479e1c6da43947c4c9d8548e4be9e05cc7f621c8
CATGPT0 <133710245+CATGPT0@users.noreply.github.com>
2024-03-11 22:35:56 -0700

    Update help panel

--- Secret of the Land/Assets/Scripts/UI/UIController.cs
+++ Secret of the Land/Assets/Scripts/UI/UIController.cs
@@ -5,11 +5,15 @@ public class UIController : MonoBehaviour
 {
     [SerializeField]
     private GameObject statPanel;
+    [SerializeField]
+    private GameObject helpPanel;
     private bool isStatPanelOpen = false;
+    private bool isHelpPanelOpen = false;
 
     void Awake()
     {
         statPanel.SetActive(false);
+        helpPanel.SetActive(false);
     }
     void Start()
     {
@@ -23,11 +27,21 @@ public class UIController : MonoBehaviour
         {
             OpenCloseStatPanel();
         }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            OpenCloseHelpPanel();
+        }
     }
 
     public void OpenCloseStatPanel()
     {
         isStatPanelOpen = !isStatPanelOpen;
         statPanel.SetActive(isStatPanelOpen);
+    }
+
+    public void OpenCloseHelpPanel()
+    {
+        isHelpPanelOpen = !isHelpPanelOpen;
+        helpPanel.SetActive(isHelpPanelOpen);
     }
 }
