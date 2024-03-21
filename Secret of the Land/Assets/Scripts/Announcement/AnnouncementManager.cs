1876112e374a638532bd2c57a27ea6e0333cf9dc
CATGPT0 <133710245+CATGPT0@users.noreply.github.com>
2024-03-20 18:52:31 -0700

    Update Hitbox

--- Secret of the Land/Assets/Scripts/Announcement/AnnouncementManager.cs
+++ Secret of the Land/Assets/Scripts/Announcement/AnnouncementManager.cs
@@ -9,6 +9,8 @@ public class AnnouncementManager : MonoBehaviour
     public AnnouncementManager Instance { get; private set; }
     public TextMeshProUGUI text;
 
+    public Coroutine currentCoroutine;
+
     void Awake()
     {
         Instance = this;
@@ -18,16 +20,19 @@ public class AnnouncementManager : MonoBehaviour
     public void ShowText(string message, float time, float alpha = 1.0f)
     {
         gameObject.SetActive(true);
-        StartCoroutine(ShowTextCoroutine(message, time, alpha));
+        if (currentCoroutine != null)
+        {
+            StopCoroutine(currentCoroutine);
+        }
+        currentCoroutine = StartCoroutine(ShowTextCoroutine(message, time, alpha));
         IEnumerator ShowTextCoroutine(string message, float time, float alpha)
         {
             text.text = message;
             text.color = new Color(text.color.r, text.color.g, text.color.b, alpha);
             yield return new WaitForSeconds(time);
             text.text = "";
+            gameObject.SetActive(false);
         }
-        gameObject.SetActive(false);
-
         
     }
 }
