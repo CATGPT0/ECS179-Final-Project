b224b13d71e8e096f712711331748ac73b6ffc34
CATGPT0 <133710245+CATGPT0@users.noreply.github.com>
2024-03-21 01:53:44 -0700

    Update NavMesh

--- Secret of the Land/Assets/Scripts/Terrain/MapTable.cs
+++ Secret of the Land/Assets/Scripts/Terrain/MapTable.cs
@@ -12,7 +12,7 @@ public static class MapTable
 {
     public static Dictionary<string, MapLocation> locations = new Dictionary<string, MapLocation>()
     {
-        { "Forest", new MapLocation() { topLeft = new Vector2(496, 1261), bottomRight = new Vector2(558, 1210) } },
+        { "Forest", new MapLocation() { topLeft = new Vector2(-157, 282), bottomRight = new Vector2(-104, 237) } },
         { "GrassLand1", new MapLocation() { topLeft = new Vector2(1001, 0), bottomRight = new Vector2(2000, 1000) } },
         { "GrassLand2", new MapLocation() { topLeft = new Vector2(0, 1001), bottomRight = new Vector2(1000, 2000) } },
         { "Village", new MapLocation() { topLeft = new Vector2(1001, 1001), bottomRight = new Vector2(2000, 2000) } }
